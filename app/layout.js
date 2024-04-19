81c99be8b850df591c4ce08da36cc2f27e0771e5
hakurou912 <db.hakurou@gmail.com>
2024-04-19 21:13:37 +0900

    appディレクトリ内の共通UI

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import './bootstrap.css';
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div style={{minHeight:'100dvh'}}>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
